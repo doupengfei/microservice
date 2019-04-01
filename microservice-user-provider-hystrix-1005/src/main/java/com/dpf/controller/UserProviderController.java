75cb19c9b47352b79d2d64a4848243dc1ea52e0d
doupengfei <sxczdoupengfei@163.com>
2019-04-01 17:00:24 +0800

    Update UserProviderController.java

--- microservice-user-provider-hystrix-1005/src/main/java/com/dpf/controller/UserProviderController.java
+++ microservice-user-provider-hystrix-1005/src/main/java/com/dpf/controller/UserProviderController.java
@@ -54,20 +54,11 @@ public class UserProviderController {
 	 */
 	@ResponseBody
 	@GetMapping(value="/getInfo")
-	@HystrixCommand(fallbackMethod="getInfoFallback")
 	public Map<String,Object> getInfo() throws InterruptedException{
-	    Thread.sleep(1000);
-	    Map<String,Object> map=new HashMap<String,Object>();
-	    map.put("code", 200);
-	    map.put("info", "业务数据xxxxx1005");
-	    return map;
+		Thread.sleep(900);
+	    return userService.getInfo();
 	}
 	 
-	public Map<String,Object> getInfoFallback() throws InterruptedException{
-	    Map<String,Object> map=new HashMap<String,Object>();
-	    map.put("code", 500);
-	    map.put("info", "系统出错，稍后重试");
-	    return map;
-	}
+	 
 	 
 }
