8d68d26272c70d7e77f29748b921c3354c935ac7
doupengfei <sxczdoupengfei@163.com>
2019-04-01 16:58:36 +0800

    Update UserConsumerController.java

--- microservice-user-consumer-feign-80/src/main/java/com/dpf/controller/UserConsumerController.java
+++ microservice-user-consumer-feign-80/src/main/java/com/dpf/controller/UserConsumerController.java
@@ -1,10 +1,12 @@
 package com.dpf.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,10 @@ public class UserConsumerController {
 	@ResponseBody
 	public List<Users> listUsers() {
 		return userClientService.listUsers();
+	}
+	@GetMapping(value="/getInfo")
+	@ResponseBody
+	public Map<String,Object> getInfo(){
+	    return userClientService.getInfo();
 	}
 }
