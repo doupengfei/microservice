2afc1fe07cf2f1a910da169f633d9d41af22b7ad
doupengfei <sxczdoupengfei@163.com>
2019-04-01 16:57:10 +0800

    Update UserClientService.java

--- microservice-common/src/main/java/com/dpf/service/UserClientService.java
+++ microservice-common/src/main/java/com/dpf/service/UserClientService.java
@@ -1,6 +1,7 @@
 package com.dpf.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dpf.beans.Users;
 
-@FeignClient(value = "MICROSERVICE-USER")
+@FeignClient(value = "MICROSERVICE-USER",fallbackFactory=UserClientFallbackFactory.class)
 public interface UserClientService {
 
 	@GetMapping(value = "/user/listUsers")
@@ -20,4 +21,10 @@ public interface UserClientService {
 
 	@GetMapping(value = "/user/deleteUser/{id}")
 	public void deleteUser(@PathVariable("id") Integer userId);
+	/**
+	 * 获取信息
+	 * @return
+	 */
+	@GetMapping(value="/user/getInfo")
+	public Map<String,Object> getInfo();
 }
